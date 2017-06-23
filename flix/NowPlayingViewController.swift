4a8d38c75344860003f99c4074da95e79ce4e95c
Xiu Chen <xiuchen@fb.com>
2017-06-22 17:02:16 -0700

    Changes to storyboard, DeatilsViewController, MovieCell, and NowPlayingViewController

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -7,69 +7,94 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     
     
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.startAnimating()
+        activityIndicator.stopAnimating()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        // add refresh control to table view
+        tableView.insertSubview(refreshControl, at: 0)
         
+        tableView.dataSource = self
+        fetchMovies()
+        
+        
+    }
+    
+    func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+        
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=e56c35f88f0c10beeb2d2b4e559eeb2a")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-
+        
         let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                
-                // TODO: Get the array of movies
-                for movie in movies {
-                    let title = movie["title"] as! String
-                }
-                
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
-                
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         
-        // Initialize a UIRefreshControl
-        let refreshControl = UIRefreshControl()
-        
         task.resume()
     }
     
     
-    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
         
-        // ... Create the URLRequest `myRequest` ...
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
         
-        // Configure session so that completion handler is executed on main UI thread
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            
-            // ... Use the new data to update the data source ...
-            
-            // Reload the tableView now that there is new data
-            myTableView.reloadData()
-            
-            // Tell the refreshControl to stop spinning
-            refreshControl.endRefreshing()
+        
+        return cell
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
         }
-        task.resume()
+        
     }
-
-    
-    
-    
     
     
     override func didReceiveMemoryWarning() {
